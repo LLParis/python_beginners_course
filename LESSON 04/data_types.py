9bb787c720c9df1c699814bea8b23d81a19940a2
Your Name <you@example.com>
2023-12-29 10:52:32 -0800

    Work in progress

--- LESSON 04/data_types.py
+++ LESSON 04/data_types.py
@@ -22,7 +22,35 @@ print(full_name)
 full_name += "!"
 print(full_name)
 
+# casting a number to a string
+decade = str(1980)
+print(type(decade))
+print(decade)
+
+statement = "I like rock music from the" + decade + "s."
+
+# multiple lines
+multiline = '''
+Hey, how are you?    
+
+I was just checking in.   
+
+                                All good?
+
+'''
+print(multiline)
+
+# escaping special characters
+sentence = "I\"m back at work!\tHey!\n\nWhere\'s this at\\located?"
+print(sentence)
+
 # string methods
+print(first)
+print(first.lower())
+print(first.upper())
+
+
+# build a menu
 
 
 
