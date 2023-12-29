e6915ef82026146a9844479dc7b0695ffe13473a
Your Name <you@example.com>
2023-12-29 08:40:58 -0800

    Work in progress

---
+++ LESSON 04/data_types.py
@@ -0,0 +1,35 @@
+# string data type
+
+# literal assignment
+first = "London"
+last = "Paris"
+
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+# constructor function
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+
+# concatenation
+full_name = first + " " + last
+
+print(full_name)
+
+full_name += "!"
+print(full_name)
+
+# string methods
+
+
+
+
+
+
+
+
+
+
